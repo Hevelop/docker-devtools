dfa79cc4f01acd6a681021797b7b594d039de211
etessari <tessari91@gmail.com>
2019-05-24 11:23:10 +0200

    removed entrypoint retrieve xdebug remote host

--- dist/7.0/entrypoint.sh
+++ dist/7.0/entrypoint.sh
@@ -4,20 +4,12 @@ set -e
 
 # Configure Xdebug
 if [ "$XDEBUG_CONFIG" ]; then
-    XDEBUG_INI=/usr/local/etc/php/conf.d/zz-xdebug.ini
-    XDEBUG_REMOTE_HOST_CONFIG=remote_host
+    XDEBUG_INI=/usr/local/etc/php/conf.d/zz-xdebug.ini    
 
     echo "" > $XDEBUG_INI
     for config in $XDEBUG_CONFIG; do
         echo "xdebug.$config" >> $XDEBUG_INI
-    done
-
-    if [[ ! "$XDEBUG_CONFIG" =~ "$XDEBUG_REMOTE_HOST_CONFIG"  ]]; then
-        REMOTE_HOST=$(netstat -nr | grep '^0\.0\.0\.0' | awk '{print $2}')
-        echo "xdebug.remote_host=${REMOTE_HOST}" >> $XDEBUG_INI
-    fi
-
-    docker-php-ext-enable xdebug
+    done    
 fi
 
 # Execute the supplied command
