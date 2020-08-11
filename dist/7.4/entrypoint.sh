9c53c8fc52a5f3336b3f036d5f5bf09ba4fe6952
Simone Marcato <simone@hevelop.com>
2020-08-11 17:17:20 +0200

    7.1 and 7.4

---
+++ dist/7.4/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+set -e
+
+# Configure Xdebug
+if [ "$XDEBUG_CONFIG" ]; then
+    XDEBUG_INI=/usr/local/etc/php/conf.d/zz-xdebug.ini
+    
+    echo "" > $XDEBUG_INI
+    for config in $XDEBUG_CONFIG; do
+        echo "xdebug.$config" >> $XDEBUG_INI
+    done    
+fi
+
+# Execute the supplied command
+exec "$@"
